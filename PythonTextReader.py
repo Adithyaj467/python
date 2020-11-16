e62d5d2d5369b71383d4f7ef3c1a5736e109f911
Adithyaj467 <37096005+Adithyaj467@users.noreply.github.com>
2020-11-16 15:52:16 +0530

    Create PythonTextReader.py
    text to speech engine

---
+++ PythonTextReader.py
@@ -0,0 +1,6 @@
+import pyttsx3
+speaker=pyttsx3.init()
+speaker.say("<ENTER WHAT YOU WANT IT TO SAY>")
+speaker.runAndWait()
+# install python text to speech engine version 3 before running
+#more such programs will be put up here stay tuned on github/Adithyaj467
